341b646fd202bc4bfe50b540f7944f2ed364eaf2
Pokrishk <anna.kononova41@gmail.com>
2024-06-18 15:45:42 +0300

    Добавьте файлы проекта.

---
+++ EMISass/ViewModel/Helpers/BindingHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMISass.ViewModel.Helpers
+{
+    internal class BindingHelper : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
